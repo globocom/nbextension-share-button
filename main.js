5a721947a12d8b746cfb22d2a13ec7f70c9e466e
Matheus Lago Pereira <m.pereira96@hotmail.com>
2017-12-27 13:55:39 -0200

    chore: create share button and popup

---
+++ main.js
@@ -0,0 +1,42 @@
+define([
+    'require',
+    'jquery',
+    'base/js/namespace',
+    './popup'
+], function(
+    requirejs,
+    $,
+    Jupyter,
+    popup
+) {
+    'use strict'
+
+    var load_extension = function() {
+
+        //add css
+        var link = document.createElement('link')
+        link.type = 'text/css'
+        link.rel = 'stylesheet'
+        link.href = requirejs.toUrl('./share-popup.css')
+        document.getElementsByTagName('head')[0].appendChild(link)
+
+        // add the popup to the page
+        $('body').append(popup)
+
+        // add the share-notebook button to the toolbar
+        $(Jupyter.toolbar.add_buttons_group([
+            Jupyter.keyboard_manager.actions.register({
+                help   : 'Opens share-notebook popup',
+                icon   : 'fa-share-alt',
+                handler: function() {
+                    $('#share-notebook-popup').toggle()
+                    setTimeout(function() { $('#btn-share-notebook').blur() }, 500)
+                }
+            }, 'open-share-notebook-popup', 'share_notebook')
+        ])).find('.btn').attr('id', 'btn-share-notebook')
+    }
+
+    return {
+        load_ipython_extension : load_extension
+    }
+})
---
+++ popup.js
@@ -0,0 +1,8 @@
+// Creates the permalink popup
+
+define([], function() {
+    var popup = document.createElement('div')
+    popup.setAttribute('id', 'share-notebook-popup')
+
+    return popup
+})
