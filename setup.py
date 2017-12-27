7ef1b728ccca79507d4b747bd1c40d2617845ad4
Matheus Lago Pereira <m.pereira96@hotmail.com>
2017-12-27 17:56:00 -0200

    chore: create python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'nbextension-share-button',
+  packages = ['nbextension-share-button'],
+  version = '0.1',
+  description = 'A Jupyter Notebook extension that adds a share button to the toolbar.',
+  author = 'Globo.com',
+  url = 'https://github.com/globocom/nbextension-share-button/',
+  keywords = ['nbextension', 'share', 'notebook'],
+  classifiers = [],
+)
