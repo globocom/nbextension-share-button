28c266beaa7df52d612350e563cd737470b3f295
Matheus Lago Pereira <m.pereira96@hotmail.com>
2017-12-27 17:56:43 -0200

    chore: move files to nbextension-share-button folder

---
+++ nbextension-share-button/__init__.py
@@ -0,0 +1,6 @@
+def _jupyter_nbextension_paths():
+    return [dict(
+        section='notebook',
+        src='static',
+        dest='nbextension-share-button',
+        require='nbextension-share-button/main')]
