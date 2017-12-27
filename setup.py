6de7ee8052ae04116d7e5403bb304ea427efc5f6
Matheus Lago Pereira <m.pereira96@hotmail.com>
2017-12-27 20:00:12 -0200

    chore: bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
   name = 'nbextension-share-button',
-  version = '0.1.6',
+  version = '0.1.7',
   description = 'A Jupyter Notebook extension that adds a share button to the toolbar.',
   author = 'Globo.com',
   url = 'https://github.com/globocom/nbextension-share-button/',
